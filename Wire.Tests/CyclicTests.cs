f67da38f9bc07e4d73679c42c692bfc22c048fee
rogeralsing <rogeralsing@gmail.com>
2015-08-20 18:39:44 +0200

    Multi level cyclic tests

--- Wire.Tests/CyclicTests.cs
+++ Wire.Tests/CyclicTests.cs
@@ -18,16 +18,46 @@ namespace Wire.Tests
             var serializer = new Serializer(new SerializerOptions(preserveObjectReferences:true));
             var bar = new Bar();
             bar.Self = bar;
+            bar.XYZ = 234;
 
             serializer.Serialize(bar, stream);
             stream.Position = 0;
             var actual = serializer.Deserialize<Bar>(stream);
             Assert.AreSame(actual,actual.Self);
+            Assert.AreEqual(bar.XYZ,actual.XYZ);
+        }
+
+        [TestMethod]
+        public void CanSerializeMultiLevelCyclicReferences()
+        {
+            var stream = new MemoryStream();
+            var serializer = new Serializer(new SerializerOptions(preserveObjectReferences: true));
+            var a = new A();
+            var b = new B();
+            a.B = b;
+            b.A = a;
+
+
+            serializer.Serialize(a, stream);
+            stream.Position = 0;
+            var actual = serializer.Deserialize<A>(stream);
+            Assert.AreSame(actual, actual.B.A);
         }
     }
 
     public class Bar
     {
         public Bar Self { get; set; }
+        public int XYZ { get; set; }
+    }
+
+    public class A
+    {
+        public B B { get; set; }
+    }
+
+    public class B
+    {
+        public A A { get; set; }
     }
 }
