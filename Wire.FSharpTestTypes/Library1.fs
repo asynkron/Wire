14959023c6b9df3bdf2372f22a21b007075159f2
rogeralsing <rogeralsing@gmail.com>
2015-08-22 23:28:33 +0200

    FSharp tests

---
+++ Wire.FSharpTestTypes/Library1.fs
@@ -0,0 +1,12 @@
+﻿namespace Wire.FSharpTestTypes
+
+type DU1 = 
+| A of int
+| B of string * int
+and DU2 =
+| C of DU1
+| D of string
+| E of option<DU1>
+
+
+
---
+++ Wire.Tests/FSharpTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.FSharp.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wire.FSharpTestTypes;
+
+namespace Wire.Tests
+{
+    [TestClass]
+    public class FSharpTests : TestBase
+    {
+        [TestMethod]
+        public void CanSerializeSimpleDU()
+        {
+            var expected = DU1.NewA(1);
+            Serialize(expected);
+            Reset();
+            var actual = Deserialize<DU1>();
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void CanSerializeNestedDU()
+        {
+            var expected = DU2.NewC(DU1.NewA(1));
+            Serialize(expected);
+            Reset();
+            var actual = Deserialize<DU2>();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CanSerializeOptionalDU()
+        {
+            var expected = DU2.NewE(FSharpOption<DU1>.Some(DU1.NewA(1)));
+            Serialize(expected);
+            Reset();
+            var actual = Deserialize<DU2>();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CanSerializeOption()
+        {
+            var expected = FSharpOption<string>.Some("hello");
+            Serialize(expected);
+            Reset();
+            var actual = Deserialize<FSharpOption<string>>();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
