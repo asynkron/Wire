885f134352bbfcfca7cd1af518c13ee1eec6b1e2
rogeralsing <rogeralsing@gmail.com>
2016-01-17 10:47:42 +0100

    clean up

--- Wire/SerializerFactories/FSharpListSerializerFactory.cs
+++ Wire/SerializerFactories/FSharpListSerializerFactory.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
 using Wire.ValueSerializers;
 
 namespace Wire.SerializerFactories
