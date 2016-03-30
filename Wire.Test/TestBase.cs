d7519042050057a49f70dd5b43ba43b9a8f0acea
rogeralsing <rogeralsing@gmail.com>
2016-03-30 21:07:34 +0200

    compiling tests

---
+++ Wire.Test/TestBase.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace Wire.Tests
+{
+    
+    public abstract class TestBase
+    {
+        private Serializer serializer;
+        private MemoryStream stream;
+
+        public TestBase()
+        {
+            serializer = new Serializer();
+            stream = new MemoryStream();
+        }
+
+        public void Reset()
+        {
+            stream.Position = 0;
+        }
+
+        public void Serialize(object o)
+        {
+            serializer.Serialize(o, stream);
+        }
+
+        public T Deserialize<T>()
+        {
+            return serializer.Deserialize<T>(stream);
+        }
+    }
+}
