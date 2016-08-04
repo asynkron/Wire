7eebd577f37229d9b572a532b223c513915c26fc
rogeralsing <rogeralsing@gmail.com>
2016-08-04 11:37:01 +0200

    size fix

--- Wire/ValueSerializers/DateTimeSerializer.cs
+++ Wire/ValueSerializers/DateTimeSerializer.cs
@@ -6,7 +6,7 @@ namespace Wire.ValueSerializers
     public class DateTimeSerializer : SessionAwareValueSerializer<DateTime>
     {
         public const byte Manifest = 5;
-        public const int Size = sizeof(long);
+        public const int Size = sizeof(long) + sizeof(byte);
         public static readonly DateTimeSerializer Instance = new DateTimeSerializer();
 
         public DateTimeSerializer() : base(Manifest, () => WriteValueImpl, ()=> ReadValueImpl)
@@ -15,12 +15,13 @@ namespace Wire.ValueSerializers
 
         private static unsafe void WriteValueImpl(Stream stream, DateTime dateTime, SerializerSession session)
         {
-            var bytes1 = session.GetBuffer(Size+1);
+            //datetime size is 9 ticks + kind
+            var bytes1 = session.GetBuffer(Size);
             fixed (byte* b = bytes1)
                 *((long*) b) = dateTime.Ticks;
-            bytes1[Size] = (byte) dateTime.Kind;
+            bytes1[Size-1] = (byte) dateTime.Kind;
 
-            stream.Write(bytes1, 0, Size+1);
+            stream.Write(bytes1, 0, Size);
         }
 
         public static DateTime ReadValueImpl(Stream stream, DeserializerSession session)
@@ -31,10 +32,10 @@ namespace Wire.ValueSerializers
 
         private static DateTime ReadDateTime(Stream stream, DeserializerSession session)
         {
-            var buffer = session.GetBuffer(Size + 1);
-            stream.Read(buffer, 0, Size + 1);
+            var buffer = session.GetBuffer(Size);
+            stream.Read(buffer, 0, Size );
             var ticks = BitConverter.ToInt64(buffer, 0);
-            var kind = (DateTimeKind) buffer[Size]; //avoid reading a single byte from the stream
+            var kind = (DateTimeKind) buffer[Size-1]; //avoid reading a single byte from the stream
             var dateTime = new DateTime(ticks, kind);
             return dateTime;
         }
