9c99a7007be9155af384dbadaa0f8f2b9707a263
rogeralsing <rogeralsing@gmail.com>
2016-08-26 20:22:44 +0200

    perftests

---
+++ Wire.PerfTestWireOnly/Types/TypicalMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wire.PerfTest.Types
+{
+    public class TypicalMessage
+    {
+        public string StringProp { get; set; }
+        public int IntProp { get; set; }
+        public Guid GuidProp { get; set; }
+        public DateTime DateProp { get; set; }
+    }
+}
