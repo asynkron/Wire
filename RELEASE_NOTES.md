3163dd3f690c96cd1eb6520f9e6ebba02d7ae385
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2016-11-24 11:52:29 +0100

    CI through FAKE script tasks

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,2 @@
+### New in 0.9.0 (Released 2016/11/24)
+* TODO
--- Wire.FSharpTestTypes/AssemblyInfo.fs
+++ Wire.FSharpTestTypes/AssemblyInfo.fs
@@ -1,41 +1,18 @@
-﻿namespace Wire.FSharpTestTypes.AssemblyInfo
-
+﻿namespace System
+open System
 open System.Reflection
-open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[<assembly: AssemblyTitle("Wire.FSharpTestTypes")>]
-[<assembly: AssemblyDescription("")>]
-[<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("Wire.FSharpTestTypes")>]
-[<assembly: AssemblyCopyright("Copyright ©  2015")>]
-[<assembly: AssemblyTrademark("")>]
-[<assembly: AssemblyCulture("")>]
+[<assembly: AssemblyTitleAttribute("Wire")>]
+[<assembly: AssemblyProductAttribute("Wire")>]
+[<assembly: AssemblyDescriptionAttribute("Binary serializer for POCO objects")>]
+[<assembly: AssemblyCopyrightAttribute("Copyright � 2013-2016 AsynkronIT")>]
+[<assembly: AssemblyCompanyAttribute("AsynkronIT")>]
+[<assembly: ComVisibleAttribute(false)>]
+[<assembly: CLSCompliantAttribute(true)>]
+[<assembly: AssemblyVersionAttribute("0.9.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.9.0.0")>]
+do ()
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[<assembly: ComVisible(false)>]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("2d321671-6320-4dd2-b174-2773192d2a5a")>]
-
-// Version information for an assembly consists of the following four values:
-// 
-//       Major Version
-//       Minor Version 
-//       Build Number
-//       Revision
-// 
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
-
-do
-    ()
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.9.0.0"
