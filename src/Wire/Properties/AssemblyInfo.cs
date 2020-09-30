0c17070de4156d7d17d705f49a1f8bea6224285c
rogerjohansson <rogeralsing@gmail.com>
2020-09-30 11:00:08 +0200

    fix nuget

--- src/Wire/Properties/AssemblyInfo.cs
+++ src/Wire/Properties/AssemblyInfo.cs
@@ -15,9 +15,9 @@ using System.Security;
 [assembly: AssemblyTitle("Wire")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Asynkron AB")]
 [assembly: AssemblyProduct("Wire")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
