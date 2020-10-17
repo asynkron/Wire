65f24f4c84e72431c1adb7fdf1ba677b5032f40e
rogerjohansson <rogeralsing@gmail.com>
2020-10-17 21:41:03 +0200

    inline more writes

--- src/Wire/Extensions/BufferExtensions.cs
+++ src/Wire/Extensions/BufferExtensions.cs
@@ -8,33 +8,33 @@ namespace Wire.Extensions
     public static class BufferExtensions
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-         public static void WriteObjectWithManifest(this IBufferWriter<byte> stream, object? value, SerializerSession session)
+        public static void WriteObjectWithManifest(this IBufferWriter<byte> stream, object? value,
+            SerializerSession session)
         {
             if (value == null) //value is null
             {
                 NullSerializer.Instance.WriteManifest(stream, session);
+                return;
             }
-            else
+
+            if (session.Serializer.Options.PreserveObjectReferences &&
+                session.TryGetObjectId(value, out var existingId))
             {
-                if (session.Serializer.Options.PreserveObjectReferences &&
-                    session.TryGetObjectId(value, out var existingId))
-                {
-                    //write the serializer manifest
-                    ObjectReferenceSerializer.Instance.WriteManifest(stream, session);
-                    //write the object reference id
-                    ObjectReferenceSerializer.Instance.WriteValue(stream, existingId, session);
-                }
-                else
-                {
-                    var vType = value.GetType();
-                    var s2 = session.Serializer.GetSerializerByType(vType);
-                    s2.WriteManifest(stream, session);
-                    s2.WriteValue(stream, value, session);
-                }
+                //write the serializer manifest
+                ObjectReferenceSerializer.Instance.WriteManifest(stream, session);
+                //write the object reference id
+                ObjectReferenceSerializer.Instance.WriteValue(stream, existingId, session);
+                return;
             }
+
+            //TODO. check is vType is same as expected serializer, if so, use that
+            var vType = value.GetType();
+            var s2 = session.Serializer.GetSerializerByType(vType);
+            s2.WriteManifest(stream, session);
+            s2.WriteValue(stream, value, session);
         }
 
-         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void WriteObject(this IBufferWriter<byte> stream, object? value, Type valueType,
             ValueSerializer valueSerializer,
             bool preserveObjectReferences, SerializerSession session)
@@ -42,28 +42,26 @@ namespace Wire.Extensions
             if (value == null) //value is null
             {
                 NullSerializer.Instance.WriteManifest(stream, session);
+                return;
             }
-            else
+
+            if (preserveObjectReferences && session.TryGetObjectId(value, out var existingId))
             {
-                if (preserveObjectReferences && session.TryGetObjectId(value, out var existingId))
-                {
-                    //write the serializer manifest
-                    ObjectReferenceSerializer.Instance.WriteManifest(stream, session);
-                    //write the object reference id
-                    ObjectReferenceSerializer.Instance.WriteValue(stream, existingId, session);
-                }
-                else
-                {
-                    var vType = value.GetType();
-                    var s2 = valueSerializer;
-                    if (vType != valueType)
-                        //value is of subtype, lookup the serializer for that type
-                        s2 = session.Serializer.GetSerializerByType(vType);
-                    //lookup serializer for subtype
-                    s2.WriteManifest(stream, session);
-                    s2.WriteValue(stream, value, session);
-                }
+                //write the serializer manifest
+                ObjectReferenceSerializer.Instance.WriteManifest(stream, session);
+                //write the object reference id
+                ObjectReferenceSerializer.Instance.WriteValue(stream, existingId, session);
+                return;
             }
+
+            var vType = value.GetType();
+            var s2 = valueSerializer;
+            if (vType != valueType)
+                //value is of subtype, lookup the serializer for that type
+                s2 = session.Serializer.GetSerializerByType(vType);
+            //lookup serializer for subtype
+            s2.WriteManifest(stream, session);
+            s2.WriteValue(stream, value, session);
         }
     }
 }
